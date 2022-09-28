924eae36d91da773a65610d1db1fc74570b6cf2e
bryan suello <bryansuello@gmail.com>
2022-09-28 22:55:54 +0800

    9-28-22

---
+++ onlyNeedOne.js
@@ -0,0 +1,15 @@
+// You will be given an array a and a value x. All you need to do is check whether
+// the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+let num1 = [1, 2, 3, 'a'];
+let num2 = 'a';
+
+function check(a, x) {
+  return a.includes(x);
+}
+
+console.log(check(num1, num2));
