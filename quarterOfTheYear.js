d9f9a9223d1c8abc85ed829ea0ea19570770b4ab
bryan suello <bryansuello@gmail.com>
2022-09-28 13:19:03 +0800

    9-28-22

--- quarterOfTheYear.js
+++ quarterOfTheYear.js
@@ -7,14 +7,36 @@ let mo = 12;
 
 const quarterOf = month => {
   return month <= 3
-    ? 'first quarter'
+    ? 1
     : month > 3 && month <= 6
-    ? 'second quarter'
+    ? 2
     : month > 6 && month <= 9
-    ? 'third quarter'
+    ? 3
     : month > 9 && month <= 12
-    ? 'fourth quarter'
+    ? 4
     : null;
 };
 
 console.log(quarterOf(mo));
+
+// const quarterOf = m => Math.ceil(m/3);
+
+// const quarterOf = (month) => {
+//   if (month <= 3) {
+//     return 1
+//   } else if (month <= 6) {
+//     return 2
+//   } else if (month <= 9) {
+//     return 3
+//   } else if (month <= 12) {
+//     return 4
+//   }
+
+// }
+
+// const quarterOf = (month) => {
+//   // Your code here
+//   return Math.ceil(month / 3);
+// }
+
+// const quarterOf = month => Math.ceil(month / 3);
