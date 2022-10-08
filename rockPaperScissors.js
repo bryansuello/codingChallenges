16df46b4f67a218469eb5f8c63d7fefc8ae0e282
bryan suello <bryansuello@gmail.com>
2022-10-08 10:06:25 +0800

    10-8-22

---
+++ rockPaperScissors.js
@@ -0,0 +1,64 @@
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+//
+// Examples(Input1, Input2 --> Output):
+//
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+let p1 = 'rock';
+let p2 = 'paper';
+
+const rps = (p1, p2) => {
+  return p1 == p2
+    ? 'Draw!'
+    : p1 == 'scissors' && p2 == 'paper'
+    ? 'Player 1 won!'
+    : p1 == 'scissors' && p2 == 'rock'
+    ? 'Player 2 won!'
+    : p1 == 'rock' && p2 == 'paper'
+    ? 'Player 2 won!'
+    : p1 == 'rock' && p2 == 'scissors'
+    ? 'Player 1 won!'
+    : p1 == 'paper' && p2 == 'scissors'
+    ? 'Player 2 won!'
+    : p1 == 'paper' && p2 == 'rock'
+    ? 'Player 1 won!'
+    : null;
+};
+
+console.log(rps(p1, p2));
+
+
+// const rps = (p1, p2) => {
+//   if (p1 === p2) return "Draw!";
+//   var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+//   if (p2 === rules[p1]) {
+//     return "Player 1 won!";
+//   }
+//   else {
+//     return "Player 2 won!";
+//   }
+// };
+
+// const rps = (p1, p2) => {
+//   if (p1 == p2)
+//     return 'Draw!';
+//     
+//    if (p1 == 'rock' && p2 == 'scissors') 
+//      return 'Player 1 won!'
+//    else if (p1 == 'scissors' && p2 == 'paper') 
+//      return 'Player 1 won!'
+//    else if (p1 == 'paper' && p2 == 'rock') 
+//      return 'Player 1 won!'
+//    else
+//      return 'Player 2 won!';
+// };
+
+
+// const rps = (p1, p2) => {
+//   if(p1 === p2) {
+//     return 'Draw!'
+//   }; 
+//   return `Player ${/rockscissors|scissorspaper|paperrock/.test(p1+p2)? 1 : 2} won!`;
+// }
